b3f6c4b821afe881608a0b68042afb829814141a
Jeremy Moseley <jeremy@moseley.dev>
2022-06-12 18:10:41 -0700

    Progress log.

--- minimax.ts
+++ minimax.ts
@@ -1,4 +1,5 @@
 import debugFactory from "debug";
+import DraftLog from "draftLog";
 
 import { Board, BoardScore } from "./board";
 import { sortByHexagonCount } from "./score";
@@ -7,26 +8,110 @@ import { Trie } from "./trie";
 
 const debug = debugFactory("minimax");
 
+DraftLog(console);
+
+class StatusTracker {
+  private bestMove: BoardScore | undefined;
+  private bestScore: number | undefined;
+  private boardsAnalyzed: number = 0;
+  private analysisTime: number = 0;
+  private maxDepth: number = 0;
+  private currentDepth: number = 0;
+  private movesRemaining: number = 0;
+
+  private statusLine: ReturnType<typeof console.draft>;
+
+  constructor() {
+    this.statusLine = console.draft("Minimax initializing....");
+  }
+
+  addMovesRemaining(numMoves: number) {
+    this.movesRemaining += numMoves;
+    this.draw();
+  }
+
+  moveProcessed(num = 1) {
+    this.movesRemaining -= num;
+    this.draw();
+  }
+
+  updateDepth(depth: number) {
+    this.maxDepth = Math.max(this.maxDepth, depth);
+    this.currentDepth = depth;
+    this.draw();
+  }
+
+  boardAnalyzed(time: number) {
+    this.boardsAnalyzed++;
+    this.analysisTime += time;
+    this.draw();
+  }
+
+  updateScore({
+    bestMove,
+    bestScore,
+  }: {
+    bestMove: BoardScore;
+    bestScore: number;
+  }) {
+    this.bestMove = bestMove;
+    this.bestScore = bestScore;
+    this.draw();
+  }
+
+  draw() {
+    const avgTimePerBoard = Math.round(this.analysisTime / this.boardsAnalyzed);
+    this.statusLine(
+      `Moves Remaining: ${
+        this.movesRemaining
+      } Avg ms/Board: ${avgTimePerBoard} Depth: ${
+        this.currentDepth
+      } Max Depth: ${this.maxDepth} ${
+        this.bestMove
+          ? ` Minimax best move: ${this.bestMove?.word.toString()}`
+          : ""
+      }`
+    );
+  }
+}
+
 // Use minimax to find the best move
 export function miniMax(board: Board, dictionary: Trie) {
+  const statusTracker = new StatusTracker();
+
+  const startTime = performance.now();
   const moves = findAllWords(board, dictionary, "blue")
     .filter((s) => s.word.length > 5)
     .sort(sortByHexagonCount("blue"));
+  statusTracker.boardAnalyzed(performance.now() - startTime);
 
-  const maxDepth = Math.min(16 - board.blueScore + 1, 5);
+  const maxDepth = Math.min(16 - board.blueScore + 1, 3);
   debug("maxDepth", maxDepth);
 
   let alpha = -Infinity;
   let best = -Infinity;
   let bestMove: BoardScore | undefined;
+  statusTracker.addMovesRemaining(moves.length);
   for (const move of moves) {
-    const score = runMiniMax(move.board, dictionary, "red", maxDepth, alpha);
+    const score = runMiniMax(
+      statusTracker,
+      move.board,
+      dictionary,
+      "red",
+      maxDepth,
+      alpha
+    );
     if (score > best) {
       best = score;
       bestMove = move;
+      statusTracker.updateScore({
+        bestMove: move,
+        bestScore: score,
+      });
     }
     alpha = Math.max(alpha, best);
     debug("Top level", best, alpha);
+    statusTracker.moveProcessed();
   }
 
   if (!bestMove) {
@@ -39,6 +124,7 @@ export function miniMax(board: Board, dictionary: Trie) {
 }
 
 function runMiniMax(
+  statusTracker: StatusTracker,
   board: Board,
   dictionary: Trie,
   mover: "red" | "blue",
@@ -48,6 +134,7 @@ function runMiniMax(
   depth: number = 0
 ): number {
   if (debug.enabled) debug("Depth", depth, "mover", mover);
+  statusTracker.updateDepth(depth);
 
   if (mover === "blue" && board.blueScore * board.probability >= 16) {
     if (debug.enabled)
@@ -71,6 +158,7 @@ function runMiniMax(
     }
   }
 
+  const startTime = performance.now();
   const moves = findAllWords(board, dictionary, mover)
     .filter((s) => s.word.length > 4)
     .filter((s) => {
@@ -82,6 +170,8 @@ function runMiniMax(
     })
     .sort(sortByHexagonCount(mover));
 
+  statusTracker.boardAnalyzed(performance.now() - startTime);
+  statusTracker.addMovesRemaining(moves.length);
   if (moves.length === 0) {
     if (debug.enabled) debug("No moves");
     if (mover === "blue") {
@@ -96,10 +186,12 @@ function runMiniMax(
   // Recursive case
   if (mover === "blue") {
     let best = -Infinity;
+    let movesProcessed = 0;
     for (const move of moves) {
       best = Math.max(
         best,
         runMiniMax(
+          statusTracker,
           move.board,
           dictionary,
           "red",
@@ -111,17 +203,22 @@ function runMiniMax(
       );
       if (best >= beta) {
         if (debug.enabled) debug("Beta cutoff", best, beta);
+        statusTracker.moveProcessed(moves.length - movesProcessed);
         break;
       }
       alpha = Math.max(alpha, best);
+      statusTracker.moveProcessed();
+      movesProcessed++;
     }
     return best;
   } else {
     let best = Infinity;
+    let movesProcessed = 0;
     for (const move of moves) {
       best = Math.min(
         best,
         runMiniMax(
+          statusTracker,
           move.board,
           dictionary,
           "blue",
@@ -133,9 +230,12 @@ function runMiniMax(
       );
       if (best <= alpha) {
         if (debug.enabled) debug("Alpha cutoff", best, alpha);
+        statusTracker.moveProcessed(moves.length - movesProcessed);
         break;
       }
       beta = Math.min(beta, best);
+      statusTracker.moveProcessed();
+      movesProcessed++;
     }
     return best;
   }
